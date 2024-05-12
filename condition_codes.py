ae73068c3e5573bc7416b2279bbd0e8e1e2b8cd4
Denis Bakhoum <denis.bakhoum@harvy.tech>
2024-05-12 15:48:35 +0200

    refactored condition codes class

--- condition_codes.py
+++ condition_codes.py
@@ -1,41 +1,33 @@
 import emoji
 
-code_groups = {
-    range(200, 233): ":cloud_with_lightning_and_rain:",
-    range(300, 322): ":cloud_with_rain:",
-    range(500, 505): ":cloud_with_rain:",
-    range(511, 512): ":snowflake:",
-    range(520, 532): ":cloud_with_rain:",
-    range(600, 617): ":snowflake:",
-    615: ":cloud_with_snow:",
-    616: ":cloud_with_snow:",
-    range(620, 623): ":snowflake:",
-    701: ":fog:",
-    711: ":smoke:",
-    721: ":fog:",
-    range(731, 772): ":wind_face:",
-    741: ":fog:",
-    762: ":volcano:",
-    781: ":tornado:",
-    800: ":sun:",
-    801: ":sun_behind_small_cloud:",
-    range(802, 805): ":cloud:",
-}
+code_groups = [
+    (range(200, 233), ":cloud_with_lightning_and_rain:"),
+    (range(300, 322), ":cloud_with_rain:"),
+    (range(500, 532), ":cloud_with_rain:"),
+    (range(600, 623), ":snowflake:"),
+    (range(701, 762), ":fog:"),
+    (762, ":volcano:"),
+    (771, ":wind_face:"),
+    (781, ":tornado:"),
+    (800, ":sun:"),
+    (801, ":sun_behind_small_cloud:"),
+    (range(802, 805), ":cloud:"),
+]
 
-def get_emoji(code):
-    for code_range, emoji_str in code_groups.items():
-        if (
-            isinstance(code_range, range) and code in code_range
-            or code == code_range
-        ):
+def get_emoji(code: int) -> str:
+    for code_range, emoji_str in code_groups:
+        if isinstance(code_range, range):
+            if code in code_range:
+                return emoji.emojize(emoji_str)
+        elif code == code_range:
             return emoji.emojize(emoji_str)
     return None
 
-codes = {str(code): get_emoji(code) for code in range(200, 805)}
-
 class ConditionCodes:
     def __init__(self):
-        self.codes = codes
+        self.codes = {str(code): get_emoji(code) for code in range(200, 805)}
 
-    def get_condition(self, code: int):
+    def get_condition(self, code: int) -> str:
+        if code < 200 or code > 804:
+            return None
         return self.codes.get(str(code), None)
